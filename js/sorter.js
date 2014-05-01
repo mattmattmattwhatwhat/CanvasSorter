daa12077ee5ceb462f347e427f3d11c406b3c1cd
mattmattmattwhatwhat <matt.thompson0@gmail.com>
2014-05-01 15:54:34 -0400

    added rectangle drawing to CanvasSorter

--- js/sorter.js
+++ js/sorter.js
@@ -8,4 +8,16 @@ CanvasSorter = function(canvas_element) {
 CanvasSorter.prototype.clear_canvas = function() {
 	this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 }
+
+CanvasSorter.prototype.draw_rect = function(x, y, w, h) {
+	this.ctx.beginPath();
+	this.ctx.fillRect(x, y, w, h);
+	this.ctx.fill();
+}
+
+CanvasSorter.prototype.set_fill_color = function(new_color) {
+	this.ctx.fillStyle = new_color;
+}
+
+
 console.log("end of sorter.js file");
